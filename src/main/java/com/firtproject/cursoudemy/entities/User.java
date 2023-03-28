07972168bb95c8e5a32cfc0a55a270633a6418b1
Cristiano Medeiros <cdsmedeiros@gmail.com>
2023-03-27 23:48:06 -0300

    User entity and resource

---
+++ src/main/java/com/firtproject/cursoudemy/entities/User.java
@@ -0,0 +1,84 @@
+package com.firtproject.cursoudemy.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private long id;
+	private String nome;
+	private String email;
+	private String phone;
+	private String passaword;
+
+	public User () {	
+	}
+
+	public User(long id, String nome, String email, String phone, String passaword) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.phone = phone;
+		this.passaword = passaword;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassaword() {
+		return passaword;
+	}
+
+	public void setPassaword(String passaword) {
+		this.passaword = passaword;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return id == other.id;
+	}
+	
+}
